4b62d612265d8461cc752c4e96c9f6e10ef35bc1
Peter Williams <peter@newton.cx>
2019-06-20 13:37:23 -0400

    recipe/build.sh: I think we don't need any of the env customizations anymore

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,19 +2,6 @@
 
 set -ex
 
-if [[ $(uname) == Darwin ]]; then
-  export CC=clang
-  export CXX=clang++
-  export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-  export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -headerpad_max_install_names"
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-elif [ "$(uname)" == "Linux" ] ; then
-  export CPPFLAGS="-I${PREFIX}/include $CPPFLAGS"
-  export LDFLAGS="-L${PREFIX}/lib $LDFLAGS"
-fi
-
 meson builddir --prefix=$PREFIX --libdir=$PREFIX/lib
 meson configure -D enable_docs=false builddir
 ninja -v -C builddir
