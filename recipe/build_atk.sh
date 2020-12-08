4ac340541967f3d0ee3ba7e1762435c41dd4a62d
Chris Burr <christopher.burr@cern.ch>
2020-12-08 13:50:08 +0100

    Fix cross-compiling following the pattern from https://github.com/conda-forge/gdk-pixbuf-feedstock/pull/28/

--- recipe/build_atk.sh
+++ recipe/build_atk.sh
@@ -2,19 +2,46 @@
 
 set -ex
 
+# Follow the pattern of https://github.com/conda-forge/gdk-pixbuf-feedstock/pull/28/
+if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
+    unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+    (
+        mkdir -p native-build
+        pushd native-build
+
+        export CC=$CC_FOR_BUILD
+        export AR=($CC_FOR_BUILD -print-prog-name=ar)
+        export NM=($CC_FOR_BUILD -print-prog-name=nm)
+        export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+        export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+        # Unset them as we're ok with builds that are either slow or non-portable
+        unset CFLAGS
+        unset CPPFLAGS
+
+        meson --prefix=$BUILD_PREFIX ..
+        # This script would generate the functions.txt and dump.xml and save them
+        # This is loaded in the native build. We assume that the functions exported
+        # by glib are the same for the native and cross builds
+        export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
+        ninja -j$CPU_COUNT -v
+        ninja install
+        popd
+    )
+    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
+fi
+
 # necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
 # meson needs this to determine where the g-ir-scanner script is located
-export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
-export PKG_CONFIG_PATH_FOR_BUILD=$PKG_CONFIG_PATH
-
-if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
-    # We're not actually building any Python bindings and this interfers with meson itself
-    unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+export PKG_CONFIG="$BUILD_PREFIX/bin/pkg-config"
+export PKG_CONFIG_PATH_FOR_BUILD="$BUILD_PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig"
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
+  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig"
 fi
 
 meson ${MESON_ARGS} builddir --prefix=$PREFIX
-meson ${MESON_ARGS} configure -D enable_docs=false builddir
+meson configure -Denable_docs=false builddir
 ninja -v -C builddir
 ninja -C builddir install
 
