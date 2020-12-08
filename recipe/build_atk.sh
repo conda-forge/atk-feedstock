402b025204791e5fc0af56980f32204a37db41d5
Chris Burr <christopher.burr@cern.ch>
2020-12-08 11:59:33 +0100

    Fix meson cross-compilation more

--- recipe/build_atk.sh
+++ recipe/build_atk.sh
@@ -4,7 +4,7 @@ set -ex
 
 # necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
 # meson needs this to determine where the g-ir-scanner script is located
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
 
 meson ${MESON_ARGS} builddir
 meson ${MESON_ARGS} configure -D enable_docs=false builddir
